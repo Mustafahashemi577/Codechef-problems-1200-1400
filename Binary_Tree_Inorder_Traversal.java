ce12d012311f8f6e014e1e9b35ae908be4df2e57
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-08-07 15:39:56 +0430

    Add files via upload
    Binary_Tree_Inorder_Traversal problem with easy difficulty code in Java
    https://leetcode.com/problems/binary-tree-inorder-traversal/description/

---
+++ Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,18 @@
+class Binary_Tree_Inorder_Traversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+
+        inorder(root, res);
+        return res;        
+    }
+
+    private void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }        
+    
+}
