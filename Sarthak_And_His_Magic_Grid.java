1ad9d0dca2b810181bb17f9fe3d394632fe3052d
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-08-12 06:31:19 +0430

    Add files via upload
    Sarthak_And_His_Magic_Grid problem with difficulty of 1227 code in Java
    https://www.codechef.com/problems/GRIDXOR

---
+++ Sarthak_And_His_Magic_Grid.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Sarthak_And_His_Magic_Grid
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+	
+		int t = scan.nextInt();
+		
+		while(t-- > 0 ){
+		    int n = scan.nextInt();
+		    
+		    for (int i = 0 ; i < n ; i++ ) {
+		        for (int j = 0 ; j < n ; j++ ) {
+		            System.out.print( 1 + " " );
+		        }
+		        System.out.println();
+		    }
+		    
+		}
+
+	}
+}
