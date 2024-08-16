1d404c5793286ec64a0be73f20408b35e43e6dae
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-08-16 12:59:47 +0330

    Add files via upload
    Chef_Jumping problem with difficulty of 1232 code in Java
    https://www.codechef.com/problems/OJUMPS

---
+++ Chef_Jumping.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Chef_Jumping
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner (System.in);
+		long a = scan.nextLong();
+        if(a%6==0 || a%6==1 || a%6==3)
+        System.out.println("yes");
+        else
+        System.out.println("no");
+            
+        
+	}
+}
